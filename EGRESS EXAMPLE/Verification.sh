71692427c317bd52d4c545260535473ef690faac
Kriti-LNU <t-kritilnu@microsoft.com>
2022-06-10 14:11:22 +0530

    Updated

--- EGRESS EXAMPLE/Verification.sh
+++ EGRESS EXAMPLE/Verification.sh
@@ -1,10 +1,12 @@
 # Connect to AKS cluster 2
 az aks get-credentials --resource-group AKSResourceGroup --name AKSCluster2
 # Create a deployment to bring up 3 pods with app:frontend in Cluster 2
+# Apply the Deployment.yaml file
 kubectl apply -f "C:\github\Spike-on-peering-and-network-policy\EGRESS EXAMPLE\Deployment.yaml"
 # Create some pods in Cluster 2 without app:frontend (to verify network policy)
 kubectl run pod1 --image=mcr.microsoft.com/oss/nginx/nginx:1.15.5-alpine --labels "app=Anyapp" --expose --port 80
 # Deploy the internal load balancer
+# Apply internallb.yaml file
 kubectl apply -f "C:\github\Spike-on-peering-and-network-policy\EGRESS EXAMPLE\internallb.yaml"
 # Get pod-ips
 kubectl get pods -o wide --all-namespaces 
@@ -23,6 +25,7 @@ kubectl run --rm -it --image=mcr.microsoft.com/aks/fundamental/base-ubuntu:v0.0.
 curl [pod-ip]
 # Specify the external-ip of internal load balancer of cluster 2 in ipblock of egress section of cluster 1
 # Now, apply the network policy file 
+# Apply the Network policy.yaml file
 kubectl apply -f "C:\github\Spike-on-peering-and-network-policy\EGRESS EXAMPLE\Network policy.yaml"
 # Try making an egress connection to any pod with/without app:frontend in Cluster 2
 kubectl run --rm -it --image=mcr.microsoft.com/aks/fundamental/base-ubuntu:v0.0.11 testpod --labels "app=backend"
